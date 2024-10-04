f422418dd117e1fb17dddd1492ae8b70341a1392
jkdeberry <debejmac@gmail.com>
2024-10-04 00:40:10 -0400

    Updated java for number of days to display

--- src/index.js
+++ src/index.js
@@ -72,7 +72,7 @@ function displayForecast(response) {
   let forecastHtml = "";
 
   response.data.daily.forEach(function (day, index) {
-    if (index > 0 && index < 6) {
+    if (index > 0 && index < 7) {
       forecastHtml =
         forecastHtml +
         `
